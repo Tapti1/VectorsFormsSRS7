7eec1ddb7a78d87257a834feb22f2a2d2131ffce
Tapti1 <olya-gorbacheva-2002@mail.ru>
2022-12-23 09:08:34 +0800

    No reflection

---
+++ VectorsForms/LazyList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VectorsForms
+{
+    internal class LazyList
+    {
+        private List<int> _lazyId;
+        private string TableName;
+        public LazyList(string _tableName)
+        {
+            TableName = _tableName;
+        }
+        public LazyList(string _tableName,List<int> _id)
+        {
+            TableName = _tableName;
+            _lazyId = _id;
+        }
+        public DomainObject GetList(int ind)
+        {
+            VectorMapper mapper = new VectorMapper();
+            return mapper.GetById(_lazyId[ind]);
+        }
+        public int Count { get { return _lazyId.Count; } }
+    }
+}
--- VectorsForms/Triangle.cs
+++ VectorsForms/Triangle.cs
@@ -10,7 +10,66 @@ namespace VectorsForms
 {
     internal class Triangle:DomainObject
     {
-        public Vector v1, v2;
+        private int _v1_id, _v2_id;
+        private Vector? _v1, _v2;
+        public int v1_id { get { return _v1_id; } set { _v1_id = value; } }
+        public int v2_id { get { return _v2_id; } set { _v2_id = value; } }
+        public Vector? v1 { 
+            get
+            {
+                if( v1_id>0)
+                {
+                    VectorMapper mapper = new VectorMapper();
+                    _v1=mapper.GetById((int)v1_id);
+                }
+                else
+                {
+                    if (v1_id!= 0)
+                    {
+                        throw new Exception("Не могу загрузить объект");
+                    }
+                    else
+                    { 
+                        _v1=null;
+                    }
+                }
+                return _v1;
+
+            }
+            set
+            {
+                _v1 = value;
+            }
+        }
+
+        public Vector? v2
+        {
+            get
+            {
+                if (v2_id > 0)
+                {
+                    VectorMapper mapper = new VectorMapper();
+                    _v2 = mapper.GetById((int)v2_id);
+                }
+                else
+                {
+                    if (v2_id != 0)
+                    {
+                        throw new Exception("Не могу загрузить объект");
+                    }
+                    else
+                    {
+                        _v2 = null;
+                    }
+                }
+                return _v2;
+
+            }
+            set
+            {
+                _v2 = value;
+            }
+        }
 
         public Triangle(int id) : base(id)
         {
@@ -18,9 +77,7 @@ namespace VectorsForms
 
             _connection.openConnection();
             SqlCommand cmd = new SqlCommand(query, _connection.getConnection());
-            SqlDataReader reader = cmd.ExecuteReader();
-
-            int _v1_id=0, _v2_id=0;
+            SqlDataReader reader = cmd.ExecuteReader();            
 
             while (reader.Read())
             {
@@ -30,16 +87,11 @@ namespace VectorsForms
             reader.Close();
             _connection.closeConnection();
             Console.WriteLine($"Выполняется запрос {query}");
-
-            VectorMapper mapper = new VectorMapper();
-            v1 = mapper.GetById(_v1_id);
-            v2 = mapper.GetById(_v2_id);
         }
         public Triangle(int v1_id,int v2_id) : base(0)
         {
-            VectorMapper mapper = new VectorMapper();
-            v1 = mapper.GetById(v1_id);
-            v2 = mapper.GetById(v2_id);
+            this._v1_id = v1_id;
+            this._v2_id = v2_id;
         }        
     }
 }
